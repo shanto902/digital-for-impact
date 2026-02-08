e0fbc00525c8e4621799f7dd904cdf43f9b5dd08
Ashik Ali Shanto <ashikalishanto@gmail.com>
2026-02-08 20:18:40 +0600

    added gtm

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,10 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Geist, Geist_Mono } from "next/font/google";
 import { NavBar } from "@/components/layout/NavBar";
 import Footer from "@/components/layout/Footer";
 import "./globals.css";
-import { Provider } from "react-redux";
-import { store } from "@/store";
+
 import ReduxProvider from "@/components/layout/ReduxProvider";
 import ThemeWrapper from "@/components/layout/ThemeWrapper";
 
@@ -31,9 +31,25 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Script id="gtm-script" strategy="beforeInteractive">
+        {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-M4KL57RD');`}
+      </Script>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased `}
       >
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-M4KL57RD"
+            height="0"
+            width="0"
+            title="Google Tag Manager"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         <ReduxProvider>
           <ThemeWrapper>
             <NavBar>{children}</NavBar>
